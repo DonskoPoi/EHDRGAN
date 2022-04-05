f9df0680b7f17dc318cd2ce6a5ad47a26c5bc87d
DonskoPoi <m15921724761@163.com>
2022-04-05 19:54:26 +0800

    add tanh Loss

---
+++ models/customize_loss.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+from basicsr.utils.registry import LOSS_REGISTRY
+
+
+@LOSS_REGISTRY.register()
+class tanhL1(nn.Module):
+    def __init__(self):
+        super(tanh_L1Loss, self).__init__()
+    def forward(self, x, y):
+        loss = torch.mean(torch.abs(torch.tanh(x) - torch.tanh(y)))
+        return loss
+
+
+@LOSS_REGISTRY.register()
+class tanhL2(nn.Module):
+    def __init__(self):
+        super(tanh_L2Loss, self).__init__()
+    def forward(self, x, y):
+        loss = torch.mean(torch.pow((torch.tanh(x) - torch.tanh(y)), 2))
+        return loss
